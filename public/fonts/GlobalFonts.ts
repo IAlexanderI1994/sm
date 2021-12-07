6125cf128fcf65fcde253f6c6cf7a7524ac770c3
ITihonya <itihonya_js@mail.ru>
2021-12-07 13:30:13 +0300

    Commit 3 - change path  for fonts

--- public/fonts/GlobalFonts.ts
+++ public/fonts/GlobalFonts.ts
@@ -6,17 +6,17 @@ const GlobalFonts = createGlobalStyle`
     @font-face {
         font-family: 'UbuntuLight';
         src: local('Ubuntu Light'), local('Ubuntu-Light'),
-            url('/font/Ubuntu-Light.woff') format('woff'),
-            url('/font/Ubuntu-Light.woff') format('woff2');
+            url('/fonts/Ubuntu-Light.woff') format('woff'),
+            url('/fonts/Ubuntu-Light.woff') format('woff2');
         font-weight: 300;
         font-style: normal;
     }
 
     @font-face {
       font-family: 'UbuntuBoldItalic';
-      src: local('Ubuntu-Bold-Italic'), local('UbuntuBoldItalic'),
-      url('/font/Ubuntu-BoldItalic.woff') format('woff'),
-      url('/font/Ubuntu-BoldItalic.woff') format('woff2');
+      src: local('Ubuntu BoldItalic'), local('Ubuntu-BoldItalic'),
+      url('/fonts/Ubuntu-BoldItalic.woff') format('woff'),
+      url('/fonts/Ubuntu-BoldItalic.woff') format('woff2');
       font-weight: bold;
       font-style: italic;
     }
